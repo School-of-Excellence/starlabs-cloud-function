5c4bcd4f9ec193adeab71b237346f97e477c5cf0
Charan Reddy <macbook@Charan-Reddy.local>
2026-07-20 16:26:28 +0530

    development push

--- functions/components/clientissue.js
+++ functions/components/clientissue.js
@@ -1398,7 +1398,7 @@ async function sendSlackDigest() {
     message.text = "<!channel> Daily SLA report image unavailable.";
   }
 
-  const url = await getWebhookUrl('Centralised Customer Support');
+  const url = await commonService.getWebhookUrl('Centralised Customer Support');
   const res = await fetch(url, {
     method: "POST",
     headers: { "content-type": "application/json" },
